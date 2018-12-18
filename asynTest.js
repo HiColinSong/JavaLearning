fc9cdbe640c84ef76b62f08868300d51723bfe22
songyunlin <yunlin.song@jwms.com>
2018-12-18 08:31:32 +0800

    在本地仓库中放入库

---
+++ asynTest.js
@@ -0,0 +1,15 @@
+function asyncFunction (callback){
+    setTimeout(callback,200);
+    console.log('output in function asyncFunction ');
+}
+
+let color="blue";
+asyncFunction(()=>{
+    console.log('the Color is ' + color);
+    }
+);
+color='Green';
+
+
+//这里输初的是the color is Green,因为是异步的
+// exports.asyncFunction= callback =>asyncFunction(callback);
---
+++ hello.js
@@ -0,0 +1,31 @@
+// function Hello() { 
+//     var name; 
+//     this.setName = function(thyName) { 
+//         name = thyName; 
+//     }; 
+//     this.sayHello = function() { 
+//         console.log('Hello ' + name); 
+//     }; 
+// }; 
+// module.exports = Hello;
+
+exports.sayhello=function (name) { 
+    //var name;   
+    
+        console.log('Hello ' + name); 
+  
+}; 
+
+function say(word) {
+    console.log(word);
+  }
+  
+  function execute(someFunction, value) {
+    someFunction(value);
+  }
+  
+  exports.exc11=function() {
+    console.log('fff');
+    debugger;
+      execute(say, "hi, pass say function as a paramater");
+  };
